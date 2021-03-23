8264bbf15ad5aa40058fb5c63d285ae5d62967ba
Jessica <duellje@oregonstate.edu>
2021-03-22 21:36:45 -0400

    went over monkey patching

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -1,12 +1,13 @@
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const assert = require("assert");
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
 
-const originalGetWinner = utils.getWinner
-utils.getWinner = (p1, p2) => p1
+const originalGetWinner = utils.getWinner;
+//mocks the getWinner function to always return player 1
+utils.getWinner = (p1, p2) => p1;
 
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+const winner = thumbWar("Kent C. Dodds", "Ken Wheeler");
+assert.strictEqual(winner, "Kent C. Dodds");
 
-// cleanup
-utils.getWinner = originalGetWinner
+//monkey patching so if any other test needs the undetermined winner from the function they can use it
+utils.getWinner = originalGetWinner;
