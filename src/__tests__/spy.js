cfc986b4b5a45846d0a4dea785dd9d5bcaaf15e7
Jessica <duellje@oregonstate.edu>
2021-03-23 19:24:57 -0400

    spyOn with jest

--- src/__tests__/spy.js
+++ src/__tests__/spy.js
@@ -1,17 +1,24 @@
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
 
-test('returns winner', () => {
-  jest.spyOn(utils, 'getWinner')
-  utils.getWinner.mockImplementation((p1, p2) => p1)
+test("returns winner", () => {
+  //spyOn does the same thing as saving the original random value to a variable
+  jest.spyOn(utils, "getWinner");
+  // const originalGetWinner = utils.getWinner;
+  //mock function
+  //mocks the function getWinner
+  utils.getWinner.mockImplementation((p1, p2) => p1);
+  // utils.getWinner = jest.fn((p1, p2) => p1);
 
-  const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-  expect(winner).toBe('Kent C. Dodds')
+  const winner = thumbWar("Kent C. Dodds", "Ken Wheeler");
+  expect(winner).toBe("Kent C. Dodds");
   expect(utils.getWinner.mock.calls).toEqual([
-    ['Kent C. Dodds', 'Ken Wheeler'],
-    ['Kent C. Dodds', 'Ken Wheeler']
-  ])
+    ["Kent C. Dodds", "Ken Wheeler"],
+    ["Kent C. Dodds", "Ken Wheeler"],
+  ]);
 
-  // cleanup
-  utils.getWinner.mockRestore()
-})
+  //cleanup
+  //does the same thing as line 21 --> restoring the original winner from the function to be available later
+  utils.getWinner.mockRestore();
+  // utils.getWinner = originalGetWinner;
+});
