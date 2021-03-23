ff0a4b8531d0d0f08fd885d4c7d67847d814340d
Jessica <duellje@oregonstate.edu>
2021-03-23 19:32:50 -0400

    added jest.mock to mock the function

--- src/__tests__/inline-module-mock.js
+++ src/__tests__/inline-module-mock.js
@@ -1,20 +1,27 @@
-const thumbWar = require('../thumb-war')
-const utilsMock = require('../utils')
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
 
-jest.mock('../utils', () => {
+//params: path of file from the test file, function for what the mock returns
+jest.mock("../utils", () => {
   return {
-    getWinner: jest.fn((p1, p2) => p1)
-  }
-})
+    getWinner: jest.fn((p1, p2) => p1),
+  };
+});
 
-test('returns winner', () => {
-  const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-  expect(winner).toBe('Kent C. Dodds')
-  expect(utilsMock.getWinner.mock.calls).toEqual([
-    ['Kent C. Dodds', 'Ken Wheeler'],
-    ['Kent C. Dodds', 'Ken Wheeler']
-  ])
+test("returns winner", () => {
+  jest.spyOn(utils, "getWinner");
+  //mock function
+  //the jest.mock on line 4 does what line 14 was doing
+  // utils.getWinner.mockImplementation((p1, p2) => p1);
 
-  // cleanup
-  utilsMock.getWinner.mockReset()
-})
+  const winner = thumbWar("Kent C. Dodds", "Ken Wheeler");
+  expect(winner).toBe("Kent C. Dodds");
+  expect(utils.getWinner.mock.calls).toEqual([
+    ["Kent C. Dodds", "Ken Wheeler"],
+    ["Kent C. Dodds", "Ken Wheeler"],
+  ]);
+
+  //cleanup
+  utils.getWinner.mockReset();
+  // utils.getWinner.mockRestore();
+});
